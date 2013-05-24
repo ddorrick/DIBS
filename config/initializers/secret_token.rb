753f82c3bd6ffb52fe5ace7fe158ed81c4e9558c
David Orrick <ddorrick@aol.com>
2013-05-23 23:12:00 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DIBS2::Application.config.secret_token = '8e74baf5193e2c6c7046ab0be37a93ac2b3ee057ac96ffd3d9bf49939d9f452ada63d0ec8a38894d483a29096c811c4477f5d3df3312667fbca9e7e4a40696d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DIBS2::Application.config.session_store :cookie_store, key: '_DIBS 2_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DIBS2::Application.config.session_store :active_record_store
